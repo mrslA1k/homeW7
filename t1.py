ad82e18ddd6b88bb75213a27d662d92421328476
mrslA1k <94779006+mrslA1k@users.noreply.github.com>
2022-02-05 21:00:39 +0300

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,9 +1,11 @@
-print("CLI приложение, которое показывает сколько дней до НГ либо рандомнамя игрушка")
+print("Написать CLI приложение,которое умеет:")
+print("считать кол-во дней до нового года (при помощи параметра --hours также часов)")
+print("Случайно выбирает какую купить игрушку для елки с двумя параметрами")
 import click
 import datetime
 import random
-from enum import Enum
 import logging
+from enum import Enum
 
 logfile = './log_t1.log'
 
@@ -28,23 +30,27 @@ def newyear(hours):
         date2 = datetime.datetime(2023, 1, 1)
         delta = date2 - date1
         hours1 = delta.total_seconds() // 3600
+        print('кол-во дней до НГ')
         if hours:
-            click.echo(print(f'{int(hours1 // 24)} дней и {int(hours1 % 24)} часов'))
+         click.echo(print(f'{int(hours1 // 24)} дней и {int(hours1 % 24)} часов'))
         else:
-            click.echo(print(f'{int(hours1 // 24)} дней'))
-        log.info("finish program newyear")            
+         click.echo(print(f'{int(hours1 // 24)} дней'))
+        log.info("finish program newyear")
     except Exception as Ex:
       log.error(Ex)
+
 @click.command()
 def toy():
- try:
+    try:
         class Toys(Enum):
             RED = "Red Ball"
             PURPLE = "Puple Angel"
+        print('выбор игрушки')
         click.echo(print(random.choice(list(Toys)).value))
         log.info("finish program toy")
- except Exception as Ex:
-  log.error(Ex)
+
+    except Exception as Ex:
+     log.error(Ex)
 
 cli.add_command(newyear)
 cli.add_command(toy)
