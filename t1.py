20af5fbabb662263a1c2818af336b9d9893a842e
mrslA1k <94779006+mrslA1k@users.noreply.github.com>
2022-02-05 20:52:06 +0300

    Update t1.py

--- t1.py
+++ t1.py
@@ -3,37 +3,51 @@ import click
 import datetime
 import random
 from enum import Enum
+import logging
+
+logfile = './log_t1.log'
+
+log = logging.getLogger("log")
+log.setLevel(logging.INFO)
+FH = logging.FileHandler(logfile)
+basic_formater = logging.Formatter('%(asctime)s : [%(levelname)s] : %(message)s')
+FH.setFormatter(basic_formater)
+log.addHandler(FH)
 
 
 @click.group()
 def cli():
     pass
 
+
 @click.command()
 @click.option('--hours', is_flag=True)
 def newyear(hours):
-    date1= datetime.datetime.now()
-    date2=datetime.datetime(2023,1,1)
-    delta=date2-date1
-    hours1 = delta.total_seconds()// 3600
-
-    if hours:
-        click.echo(print(f'{int(hours1//24)} дней и {int(hours1%24)} часов'))
-    else:
-        click.echo(print(f'{int(hours_1//24)} дней'))
-
-cli.add_command(newyear)
-
+    try:
+        date1 = datetime.datetime.now()
+        date2 = datetime.datetime(2023, 1, 1)
+        delta = date2 - date1
+        hours1 = delta.total_seconds() // 3600
+        if hours:
+            click.echo(print(f'{int(hours1 // 24)} дней и {int(hours1 % 24)} часов'))
+        else:
+            click.echo(print(f'{int(hours1 // 24)} дней'))
+        log.info("finish program newyear")            
+    except Exception as Ex:
+      log.error(Ex)
 @click.command()
 def toy():
-    class Toys(Enum):
-        RED = "Red Ball"
-        PURPLE = "Puple Angel"
+ try:
+        class Toys(Enum):
+            RED = "Red Ball"
+            PURPLE = "Puple Angel"
+        click.echo(print(random.choice(list(Toys)).value))
+        log.info("finish program toy")
+ except Exception as Ex:
+  log.error(Ex)
 
-    click.echo(print(random.choice(list(Toys)).value))
-
+cli.add_command(newyear)
 cli.add_command(toy)
-
 
 if __name__ == '__main__':
     cli()
